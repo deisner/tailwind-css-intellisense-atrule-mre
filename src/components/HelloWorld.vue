f841def8a435e863f59283c8dc36bda9ef637d45
David Eisner <deisner@gmail.com>
2024-06-25 19:01:13 +0000

    Add Tailwind, demonstrate @apply warning in SFC

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,9 +9,9 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      <span class="text-blue-500">You’ve successfully created a project with</span>
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
@@ -24,6 +24,7 @@ h1 {
   font-size: 2.6rem;
   position: relative;
   top: -10px;
+  @apply text-orange-500;
 }
 
 h3 {
